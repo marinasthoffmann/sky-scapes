ee34a305e43f064e65a269d835f02ea137c923cc
Marina Hoffmann <marinasthoffmann@gmail.com>
2024-03-20 08:43:03 +0100

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
+import Header from "./components/Header"
 
 const GradientBackground = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
     width: 100%;
     min-height: 100vh;
+    img {
+      max-width: 212px;
+    }
 `
 
 function App() {
@@ -12,6 +16,7 @@ function App() {
   return (
     <GradientBackground>
       <GlobalStyles />
+      <Header />
     </GradientBackground>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import TextField from "../TextField"
+
+const StyledHeader = styled.header`
+    padding: 60px 0;
+    margin-inline: 20px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Header = () => {
+    return(
+        <StyledHeader>
+            <img src="/images/logo.png" alt=""/>
+            <TextField />
+        </StyledHeader>
+    )
+}
+
+export default Header
