55a76de7b127b720c34c33a3228bb3025a137f19
Marina Hoffmann <marinasthoffmann@gmail.com>
2024-03-20 09:40:12 +0100

    create banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 import Header from "./components/Header"
 import SideBar from "./components/SideBar"
+import Banner from "./components/Banner"
 
 const GradientBackground = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -19,6 +20,7 @@ function App() {
       <GlobalStyles />
       <Header />
       <SideBar />
+      <Banner text={"The most complete space photo gallery!"} backgroundImage={"./images/banner.png"}/>
     </GradientBackground>
   )
 }
