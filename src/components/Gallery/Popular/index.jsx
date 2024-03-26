56e5ad1b2cf81b215445caeb92e3da57a5ff6695
Marina Hoffmann <marinasthoffmann@gmail.com>
2024-03-26 09:10:21 +0100

    create popular bar

--- src/components/Gallery/Popular/index.jsx
+++ src/components/Gallery/Popular/index.jsx
@@ -1,8 +1,41 @@
+import styled from "styled-components"
 import Title from "../../Title"
+
+import photos from './photos-popular.json'
+
+const PhotosColumn = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`
+
+const Photo = styled.img`
+    max-width: 212px;
+    border-radius: 20px;
+`
+
+const Button = styled.button`
+    background-color: transparent;
+    color: #fff;
+    border: 2px solid;
+    border-color: #C98CF1;
+    padding: 12px 20px;
+    font-size: 20px;
+    border-radius: 10px;
+    cursor: pointer;
+    width: 100%;
+    margin-top: 16px;
+`
 
 const Popular = () => {
     return (
-        <Title aligment="center">Popular</Title>
+        <section>
+            <Title aligment="center">Popular</Title>
+            <PhotosColumn>
+                {photos.map(photo => <Photo key={photo.id} src={photo.path} alt={photo.alt} />)}
+            </PhotosColumn>
+            <Button>View more</Button>
+        </section>
     )
 }
 
